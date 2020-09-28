2beececbf4b50c742556505336a1c917c1879272
Yannick <20306025+yandeu@users.noreply.github.com>
2020-09-28 17:08:03 +0200

    .

--- client.js
+++ client.js
@@ -10,16 +10,7 @@ const toFixed = (n) => {
 const main = () => {
 
   let dataChannel
-  let player = {
-    id: Math.random().toString(36).substr(2, 9),
-    color: Math.random() * 0xffffff,
-    x: toFixed(Math.random() * 800),
-    y: toFixed(Math.random() * 600),
-    velocity: {
-      x: toFixed((Math.random() * 2 - 1) * 2),
-      y: toFixed((Math.random() * 2 - 1) * 2)
-    }
-  }
+
 
   const onDataChannel = (ev) => {
     const { channel } = ev
@@ -101,11 +92,26 @@ const main = () => {
       dataChannel.send(JSON.stringify({ 'update': update }))
     }
 
+    let player = {
+      id: Math.random().toString(36).substr(2, 9),
+      color: Math.random() * 0xffffff,
+      x: toFixed(Math.random() * 800),
+      y: toFixed(Math.random() * 600),
+      velocity: {
+        x: toFixed((Math.random() * 2 - 1) * 2),
+        y: toFixed((Math.random() * 2 - 1) * 2)
+      }
+    }
+
 
     setInterval(() => {
 
-      if (player.x > 800 || player.x < 0) player.velocity.x = -player.velocity.x
-      if (player.y > 600 || player.y < 0) player.velocity.y = -player.velocity.y
+      if (player.x >= 800) player.velocity.x = -Math.abs(player.velocity.x)
+      if (player.x <= 0) player.velocity.x = Math.abs(player.velocity.x)
+
+      if (player.y >= 600) player.velocity.y = -Math.abs(player.velocity.y)
+      if (player.y <= 0) player.velocity.y = Math.abs(player.velocity.y)
+
 
       player.x += player.velocity.x
       player.y += player.velocity.y
@@ -113,8 +119,16 @@ const main = () => {
       player.x = toFixed(player.x)
       player.y = toFixed(player.y)
 
+      const buffer = new ArrayBuffer(8) // 2 * 32 bit = 64 bit = 8 bytes
+      const view = new DataView(buffer);
+      view.setInt32(0, player.x * 100);
+      view.setInt32(4, player.y * 100);
 
-      sendMessage({ player })
+      //console.log('client', player.x, player.y)
+
+      dataChannel.send(buffer)
+
+      //sendMessage({ player })
     }, 1000 / 30)
 
 
@@ -127,8 +141,10 @@ const main = () => {
   }, 1000)
 }
 
-const TOTAL_CONNECTIONS = 4
+const TOTAL_CONNECTIONS = 64
 
 for (let i = 0; i < TOTAL_CONNECTIONS; i++) {
-  main()
+  setTimeout(() => {
+    main()
+  }, Math.random() * 10_000)
 }
--- server.js
+++ server.js
@@ -10,18 +10,42 @@ io.listen()
 
 setInterval(() => {
   console.log('total connection: ', io.connectionsManager.connections.size)
-}, 1000)
+}, 5000)
 
 let updates = []
 
 setInterval(() => {
-  io.emit('updates', updates)
+
+  const u = [...updates]
   updates = []
+
+
+
+
+  const buffer = new ArrayBuffer(u.length * 8)
+  const view = new DataView(buffer);
+
+  //console.log(u)
+
+  for (let i = 0; i < u.length; i++) {
+    view.setInt32(i * 8, u[i].x)
+    view.setInt32(i * 8 + 4, u[i].y)
+  }
+
+  // console.log(buffer.byteLength)
+
+  io.raw.emit(buffer)
+
 }, 1000 / 30);
 
 io.onConnection(channel => {
-  channel.on('update', data => {
-    updates.push(data)
+  channel.onRaw(buffer => {
+
+    const view = new DataView(buffer);
+    const x = +view.getInt32(0)
+    const y = +view.getInt32(4)
+
+    updates.push({ x, y })
   })
 })
 
