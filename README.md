257c947e679dd3ef1e8fc12bbea6af582c3b8240
yandeu <20306025+yandeu@users.noreply.github.com>
2020-10-13 15:31:21 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # geckos.io stress test
+
+Based on what I can observe (tested locally), the **server** runs more or less stable with:
+
+- 30 connection with 60 fps
+- 32 connection with 50 fps
+- 38 connection with 40 fps
+- 44 connection with 30 fps
+- 50 connection with 20 fps
+
+The byteSize you send has no impact on performance.
+
+---
+
+Why this limit? I guess because `dataChannel.send()` is so slow. It takes about 0.2-0.3 ms to send a message.
+
+Unfortunately, for now, the DataChannels do not run in a worker. So that is not an option for now.
+
+I guess a node.js cluster would help, but for now, geckos.io does not work in a cluster.
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const geckos = require('@geckos.io/server').default
 const { iceServers } = require('@geckos.io/server')
 
+const FPS = 1000 / 40
 
 const io = geckos({
   iceServers: process.env.NODE_ENV === 'production' ? iceServers : iceServers
@@ -8,44 +9,37 @@ const io = geckos({
 
 io.listen()
 
-setInterval(() => {
-  console.log('total connection: ', io.connectionsManager.connections.size)
-}, 5000)
-
 let updates = []
+let start = new Date().getTime()
 
 setInterval(() => {
-
   const u = [...updates]
   updates = []
 
-
-
-
   const buffer = new ArrayBuffer(u.length * 8)
-  const view = new DataView(buffer);
-
-  //console.log(u)
+  const view = new DataView(buffer)
 
   for (let i = 0; i < u.length; i++) {
     view.setInt32(i * 8, u[i].x)
     view.setInt32(i * 8 + 4, u[i].y)
   }
 
-  // console.log(buffer.byteLength)
-
   io.raw.emit(buffer)
 
-}, 1000 / 30);
+  let end = new Date().getTime()
+
+  const realFps = Math.round(FPS - (end - start))
+  if (realFps < -2) console.log(realFps)
+
+  start = end
+}, FPS)
 
 io.onConnection(channel => {
   channel.onRaw(buffer => {
-
-    const view = new DataView(buffer);
+    const view = new DataView(buffer)
     const x = +view.getInt32(0)
     const y = +view.getInt32(4)
 
     updates.push({ x, y })
   })
 })
-
